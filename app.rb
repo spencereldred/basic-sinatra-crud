51a0cd45505ec028f7de3caaf9c956ee6ebad492
Spencer Eldred <eldredspencer@gmail.com>
2014-07-14 11:30:51 -0600

    initial commit

---
+++ app.rb
@@ -0,0 +1,14 @@
+require "sinatra"
+require "active_record"
+require "./lib/database_connection"
+
+class App < Sinatra::Application
+  def initialize
+    super
+    @database_connection = DatabaseConnection.establish(ENV["RACK_ENV"])
+  end
+
+  get "/" do
+    "Hello"
+  end
+end
